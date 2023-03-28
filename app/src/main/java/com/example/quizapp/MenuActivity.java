639df1f36454435786b3299943e09fd770401ecb
Kónya Donát <konya.donat@gmail.com>
2023-03-28 20:32:46 +0200

    Signout added

--- app/src/main/java/com/example/quizapp/MenuActivity.java
+++ app/src/main/java/com/example/quizapp/MenuActivity.java
@@ -2,13 +2,36 @@ package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MenuActivity extends AppCompatActivity {
 
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        mAuth = FirebaseAuth.getInstance();
+        TextView icon = (TextView) findViewById(R.id.usericon);
+
+        icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(MenuActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
     }
 }
