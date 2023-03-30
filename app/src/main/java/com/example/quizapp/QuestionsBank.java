d81d3966e616fca05461c1cf8b766ab737a7c4b6
Kónya Donát <konya.donat@gmail.com>
2023-03-31 00:20:19 +0200

    question minor fix

--- app/src/main/java/com/example/quizapp/QuestionsBank.java
+++ app/src/main/java/com/example/quizapp/QuestionsBank.java
@@ -9,7 +9,7 @@ public class QuestionsBank {
     private static List<QuestionList>  konyha() {
         List<QuestionList> questionLists = new ArrayList<>();
 
-        QuestionList question1 = new QuestionList("Főzőlap","desk","cutting board","hot plate","oven","cutting board","");
+        QuestionList question1 = new QuestionList("Főzőlap","desk","cutting board","hot plate","oven","hot plate","");
         QuestionList question2 = new QuestionList("Konyha kés","Kitchen knife","Cuttion board","Fridgerator","valami","Kitchen knife","");
         questionLists.add(question1);
         questionLists.add(question2);
