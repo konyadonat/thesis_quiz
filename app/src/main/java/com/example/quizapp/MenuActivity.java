77040d6763f71c2f0ba5180652cba71b2b255a94
Kónya Donát <konya.donat@gmail.com>
2023-04-03 18:19:11 +0200

    menuactivity konyha topic implemented

--- app/src/main/java/com/example/quizapp/MenuActivity.java
+++ app/src/main/java/com/example/quizapp/MenuActivity.java
@@ -22,6 +22,23 @@ public class MenuActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         TextView icon = (TextView) findViewById(R.id.usericon);
 
+        Button konyhabutton = findViewById(R.id.konyhabutton);
+
+        //TODO ETTEREM, GYM, KARACSONY
+        Button etterembutton = findViewById(R.id.etterembutton);
+        Button edzoterembutton = findViewById(R.id.edzoterembutton);
+        Button karacsonybutton = findViewById(R.id.karacsonybutton);
+
+        konyhabutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this,QuizActivity.class);
+                intent.putExtra("topic","konyha");
+                startActivity(intent);
+            }
+        });
+
+
         icon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
