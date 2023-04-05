07f0a7f4272d0c1bdcd9afc7df2d61f2196477ab
Kónya Donát <konya.donat@gmail.com>
2023-04-05 14:29:10 +0200

    Todos marked, basic structure built

--- app/src/main/java/com/example/quizapp/ResultsActivity.java
+++ app/src/main/java/com/example/quizapp/ResultsActivity.java
@@ -10,7 +10,14 @@ import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.dynamic.IFragmentWrapper;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class ResultsActivity extends AppCompatActivity {
+    FirebaseUser firebaseUser;
+
+    FirebaseAuth mAuth;
     Button resultsButton;
     private TextView result;
     @Override
@@ -18,11 +25,40 @@ public class ResultsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
 
+        mAuth = FirebaseAuth.getInstance();
+        String topic =  getIntent().getStringExtra("topic");
+        firebaseUser = mAuth.getCurrentUser();
         result = findViewById(R.id.correctanswersresult);
         int correct = getIntent().getIntExtra("correct",0);
         int total = getIntent().getIntExtra("total",0);
-
         double szazalek = (double)correct / (double)total;
+
+        if (topic == "Konyha") {
+
+        }
+
+        switch (topic){
+            case "Konyha":
+                //Todo//TODO temp String with szazalek value
+                //            //TODO user.set..lvl1besttry(temp)
+                            if (szazalek >= 0.8) {
+                                //Todo user.setpassedlvl1 = "true"
+                            }
+            case "Etterem":
+                if (szazalek >= 0.8) {
+                    //Todo user.setpassedlvl2 = "true"
+                }
+            case "Gym" :
+                if (szazalek >= 0.8) {
+                    //Todo user.setpassedlvl3 = "true"
+                }
+            case "Karacsony" :
+                if (szazalek >= 0.8) {
+                    //Todo user.setpassedlvl3 = "true"
+                }
+            default: break;
+        }
+
         result.setText("Jó válasz: " + correct+ " !" + "Totál %: " +szazalek);
 
         resultsButton = findViewById(R.id.resultsbutton);
