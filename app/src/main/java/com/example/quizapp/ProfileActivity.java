b66b6cdc94755c6da0a206e889cd59936979b8e5
Kónya Donát <konya.donat@gmail.com>
2023-04-07 13:46:17 +0200

    Profile activity backend implemented

--- app/src/main/java/com/example/quizapp/ProfileActivity.java
+++ app/src/main/java/com/example/quizapp/ProfileActivity.java
@@ -1,16 +1,27 @@
 package com.example.quizapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    FirebaseUser currentUser;
     FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +30,44 @@ public class ProfileActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        //TODO VALIDATION IF USER CHANGES DATA
+        currentUser = LoginActivity.currentuser;
+
         Button logout = findViewById(R.id.profilelogutbutton);
+        Button backbutton = findViewById(R.id.profilebackbutton);
+        EditText emailet = findViewById(R.id.profileemail);
+        EditText scoreet = findViewById(R.id.profilescore);
+
+        User user = new User(mAuth.getCurrentUser().getEmail());
+
+        DatabaseReference userReference = FirebaseDatabase.getInstance("https://steng-dab96-default-rtdb.europe-west1.firebasedatabase.app/").getReference().child("Users/");
+
+        Query checkUser = userReference.orderByChild("username").equalTo(user.getUsername());
+
+        checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    String email = snapshot.child(user.getUsername()).child("email").getValue(String.class);
+                    String score = snapshot.child(user.getUsername()).child("score").getValue(String.class);
+                    scoreet.setText(score);
+                    emailet.setText(email);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this,MenuActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
